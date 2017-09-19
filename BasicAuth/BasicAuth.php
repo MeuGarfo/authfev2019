b3aa3ed466a58794b36177214abc867538fcde36
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-09-19 15:11:52 -0300

    M  BasicAuth/BasicAuth.php M  README.md M  composer.json A  composer.lock

--- BasicAuth/BasicAuth.php
+++ BasicAuth/BasicAuth.php
@@ -2,22 +2,144 @@
 /**
  * User: Anderson Ismael
  * Date: 19/set/2017
+ * 
+ * Signin erros:
+ * invalid_email
+ * invalid_password
+ * 
+ * Signup erros:
+ * invalid_name
+ * invalid_email
+ * invalid_password
  */
 namespace BasicAuth;
 
+use Medoo\Medoo;
+
 class BasicAuth
 {
+    private $db;
+    function __construct($db){
+        $this->setDb(($db);
+    }
+    private function setDb($db){
+        $this->db = new Medoo([
+            // required
+            'database_type' => 'mysql',
+            'database_name' => $db['name'],
+            'server' => $db['server'],
+            'username' => $db['user'],
+            'password' => $db['password'],
+            // [optional]
+            'charset' => 'utf8',
+            'port' => 3306
+        ]);
+    }
     public function isAuth(){
-        
+        if(!isset($_COOKIE['id'])){
+            return false;
+        }
+        if(!isset($_COOKIE['toke'])){
+            return false;
+        }
+        $where['AND']=[
+            'id'=>@$_COOKIE['id'],
+            'token'=>@$_COOKIE['token']
+        ];
+        $user=$this->db->get('user','*', $where);
+        if (isset($user['token_expiration'])) {
+            if ($user['token_expiration']>time()) {
+                return $user;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
     public function logout(){
-        
+        $user=$this->isAuth();
+        setcookie("token", "", time()-3600,'/');
+        setcookie("id", "", time()-3600,'/');
+        if($user){
+            $data=[
+                'token_expiration'=>time()-3600
+            ];
+            $this->db->update('user', $data, ['id'=>$user['id']]);
+        }
     }
     public function signin(){
-        
+        $this->logout();
+        $email=@$_POST['email'];
+        $password=@$_POST['password'];
+        $error=false;
+        $where=[
+            'email'=>$email
+        ];
+        $user=$this->db->get('user','*',$where);
+        if(!$user){
+            $error[]='invalid_email'
+        }
+        if (password_verify($password, $user['password'])) {
+            $id=$user['id'];
+            $min=60;
+            $hora=60*$min;
+            $dia=24*$hora;
+            $ano=365*$dia;
+            $limit=time()+(2*$ano);
+            $token=bin2hex(openssl_random_pseudo_bytes(32));
+            $data=[
+                'token'=>$token,
+                'token_expiration'=>$limit
+            ];
+            $this->db->update('user', $data, ['id'=>$id]);
+            setcookie("id", $id, $limit,'/');
+            setcookie("token", $token, $limit,'/');
+            return $this->db->get("user","*",['id'=>$id]);
+        } else {
+            $error[]='invalid_password';
+        }
+        if($error){
+            return ['error'=>$error];
+        }
     }
     public function signup(){
-        
+        $this->logout();
+        $user['created_at']=time();
+        $user['name']=trim($_POST['name']);
+        $user['name']=strtolower($user['name']);
+        $user['name']=ucfirst($user['name']);
+        $user['name']=preg_replace('/\s+/', ' ',$user['name']);
+        $user['email']=@$_POST['email'];
+        $user['password']=@$_POST['password'];
+        $error=false;
+        if(preg_match('/^[a-z0-9 .\-]+$/i', $user['name']) && strlen($user['name'])>=3){
+            if (filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
+                if(strlen($user['password'])>=8){
+                    $user['password']=password_hash($user['password'], PASSWORD_DEFAULT);
+                    if ($this->db->get('user','*', ['email'=>$user['email']])) {
+                        $error[]='invalid_email';
+                    } else {
+                        $user=[
+                            'email'=>$user['email'],
+                            'name'=>$user['name'],
+                            'password'=>$user['password']
+                        ];
+                        $this->db->insert('user', $user);
+                        return $this->signin();
+                    }
+                }else{
+                    $error[]='invalid_password'
+                }
+            }else{
+                $error[]='invalid_email';
+            }
+        }else{
+            $error[]='invalid_name';
+        }
+        if($error){
+            return ['error'=>$error];
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # basicauth
 :lock: Sistema básico de autenticação
+
+## Tabela user
+```
+id
+email
+name
+password
+token
+token_expiration
+```
+
+## Erros de signin
+- invalid_email
+- invalid_password
+
+## Erros de signup
+- invalid_name
+- invalid_email
+- invalid_password
